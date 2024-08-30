2203fce105d5186e731f56206ad843a95999c4c4
Susan Don <susankutu88@gmail.com>
2024-08-30 13:56:13 +0545

    first

---
+++ main.js
@@ -0,0 +1,62 @@
+// let a = 1;
+// let b = "susan";
+
+// const c = "constant value";
+
+// console.log("hello");
+
+//========================templer literal
+// const name = "susan";
+// const address = "usa";
+
+// const sentence = `i am ${name}  and i live in ${address}`;
+
+//===================spread operator ...
+
+// const user = {
+//   name: "Susan",
+//   age: 36,
+// };
+// const address = {
+//   city: "KTM",
+//   country: "napela",
+// };
+
+// const completeUser = { ...user, ...address };
+// console.log(completeUser);
+
+//=================rest operator
+
+//==========map===========
+// const arr = [3, 54, 6, 76];
+// const a = arr.map((item) => {
+//   const calcu = item + 1;
+//   return calcu;
+// });
+// console.log(a);
+
+//filter
+
+// users = [
+//   { name: "ram", age: 34 },
+//   { name: "Fam", age: 4 },
+//   { name: "Sam", age: 24 },
+// ];
+
+// const age = users.filter((item) => {
+//   const condition = item.age > 18;
+//   return condition;
+// });
+
+// console.log(age);
+
+// //find
+
+document.getElementById("myBtn").addEventListener("click", function () {
+  document.getElementById("myBtn").style.backgroundColor = "Green";
+
+  document.getElementById("myBtn").innerHTML = "CLICKED";
+
+  document.getElementById("one").innerHTML = "Ronaldo";
+});
+
